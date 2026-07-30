1613aef45b502533eeee02518b84ec032072f0d2
PencilFluke <willhobden93@gmail.com>
2026-07-31 00:14:07 +0100

    Player movement, Constraints & Collectible spin

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    
+    [SerializeField]
+    private float spinRate = 200.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up * Time.deltaTime * spinRate, Space.World);
     }
 }
